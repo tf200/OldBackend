c0f95fa21ae58865baacb82d761f8df6a7af7f46
Taha <contact@aiautomationagency.ma>
2024-01-31 11:23:10 +0100

    domain name

--- healty/settings.py
+++ healty/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vrw!mq7f#wac=db+jgzumdn-slvi@ce7*@-!ks3!6)1*#dx!&=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.142.18.151' , 'ec2-3-142-18-151.us-east-2.compute.amazonaws.com', '127.0.0.1' ]
+ALLOWED_HOSTS = ['3.142.18.151' , 'ec2-3-142-18-151.us-east-2.compute.amazonaws.com', '127.0.0.1' , 'api.aichatkings.com' ]
 
 
 # Application definition
