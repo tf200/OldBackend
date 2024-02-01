8e549f22357c91e538f5ef7c35d464e64a806b2d
Taha <contact@aiautomationagency.ma>
2024-02-01 15:27:55 +0100

    ordering

--- client/filters.py
+++ client/filters.py
@@ -1,5 +1,6 @@
 from django_filters import rest_framework as filters
 from .models import ClientDiagnosis
+from django_filters.filters import DateFilter , DateFromToRangeFilter
 
 
 
@@ -11,6 +12,16 @@ class ClientDiagnosisFilter(filters.FilterSet):
     status = filters.CharFilter(lookup_expr='icontains')
     diagnosing_clinician = filters.CharFilter(lookup_expr='icontains')
 
+    # Date filters
+    date_of_diagnosis = DateFilter()
+    date_of_diagnosis_range = DateFromToRangeFilter()
+    date_of_diagnosis_year = DateFilter(field_name='date_of_diagnosis', lookup_expr='year')
+    date_of_diagnosis_month = DateFilter(field_name='date_of_diagnosis', lookup_expr='month')
+    date_of_diagnosis_day = DateFilter(field_name='date_of_diagnosis', lookup_expr='day')
+
     class Meta:
         model = ClientDiagnosis
-        fields = ['title', 'diagnosis_code', 'description', 'severity', 'status', 'diagnosing_clinician']
+        fields = ['title', 'diagnosis_code', 'description', 'severity', 'status', 
+                  'diagnosing_clinician', 'date_of_diagnosis', 
+                  'date_of_diagnosis_range', 'date_of_diagnosis_year', 
+                  'date_of_diagnosis_month', 'date_of_diagnosis_day']
