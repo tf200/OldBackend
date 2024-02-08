1b2c757c9f45557e70d959a7bb7486df98ec5480
001oussama01 <a2468ad@gmail.com>
2024-02-08 09:30:37 +0100

    crud operations

--- employees/views.py
+++ employees/views.py
@@ -1,17 +1,13 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
-from .serializers import UserEmployeeProfileSerializer , ClientprogressSerializer , MeasurementSerializer
-from rest_framework.response import Response
-from client.tasks import send_progress_report_email
-from .models import ProgressReport , EmployeeProfile,Measurement
+from .serializers import EmotionalStateSerializer, FeedbackSerializer, ObservationsSerializer, PhysicalStateSerializer, UserEmployeeProfileSerializer , ClientprogressSerializer , MeasurementSerializer
+from .models import EmotionalState, Feedback, Observations, PhysicalState, ProgressReport , EmployeeProfile,Measurement
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticated
+from client.tasks import send_progress_report_email
 from rest_framework.filters import OrderingFilter
 from client.pagination import CustomPagination
+from rest_framework.response import Response
+from rest_framework import generics
+from django.shortcuts import render
 
 
 
@@ -23,7 +19,6 @@ class CurrentUserProfileView(generics.RetrieveAPIView):
     def get(self, request):
         serializer = UserEmployeeProfileSerializer(request.user)
         return Response(serializer.data)
-
 
 
 
@@ -66,7 +61,6 @@ class ProgressReportDeleteView(generics.DestroyAPIView):
 
 
 
-
 #=====================================================
 class ClientMeasurmentRUDView (generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated]
@@ -90,3 +84,99 @@ class ClientMeasurmentListView(generics.ListAPIView):
     def get_queryset(self):
         client_id = self.kwargs['client']
         return Measurement.objects.filter(client=client_id)
+    
+
+
+#=====================================================
+class ClientObservationsRUDView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ObservationsSerializer
+    queryset = Observations.objects.all()
+
+class ClientObservationsCreateView(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ObservationsSerializer
+
+class ClientObservationsListView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ObservationsSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    ordering_fields = ['client', 'date']
+    ordering = ['date']
+    pagination_class = CustomPagination
+
+    def get_queryset(self):
+        client_id = self.kwargs['client']
+        return Observations.objects.filter(client=client_id)
+    
+
+
+#=====================================================
+class ClientFeedbackRUDView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = FeedbackSerializer
+    queryset = Feedback.objects.all()
+
+class ClientFeedbackCreateView(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = FeedbackSerializer
+
+class ClientFeedbackListView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = FeedbackSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    ordering_fields = ['client', 'date']
+    ordering = ['date']
+    pagination_class = CustomPagination
+
+    def get_queryset(self):
+        client_id = self.kwargs['client']
+        return Feedback.objects.filter(client=client_id)
+    
+
+
+#=====================================================
+class ClientEmotionalStateRUDView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = EmotionalStateSerializer
+    queryset = EmotionalState.objects.all()
+
+class ClientEmotionalStateCreateView(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = EmotionalStateSerializer
+
+class ClientEmotionalStateListView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = EmotionalStateSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    ordering_fields = ['client', 'date']
+    ordering = ['date']
+    pagination_class = CustomPagination
+
+    def get_queryset(self):
+        client_id = self.kwargs['client']
+        return EmotionalState.objects.filter(client=client_id)    
+    
+
+
+#=====================================================
+class ClientPhysicalStateRUDView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PhysicalStateSerializer
+    queryset = PhysicalState.objects.all()
+
+class ClientPhysicalStateCreateView(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PhysicalStateSerializer
+
+class ClientPhysicalStateListView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PhysicalStateSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    ordering_fields = ['client', 'date']
+    ordering = ['date']
+    pagination_class = CustomPagination
+
+    def get_queryset(self):
+        client_id = self.kwargs['client']
+        return PhysicalState.objects.filter(client=client_id)        
