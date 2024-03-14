f937350b4461d857a51b8c707957225573b42bc8
aiautomationagency <taha@DESKTOP-E0ANVC4>
2024-03-14 12:39:40 +0000

    test

--- authentication/models.py
+++ authentication/models.py
@@ -2,16 +2,31 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Location(models.Model):
+    name = models.CharField(max_length = 100)
+    adress = models.CharField(max_length = 100)
+
 class CustomUser(AbstractUser):
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
     phone_number = models.IntegerField(null= True , blank = True)
+    location = models.ForeignKey(Location ,  on_delete=models.SET_NULL, related_name='location' , null= True)
     
 
 
 
-class adresses(models.Model):
-    user = models.ForeignKey(CustomUser , on_delete = models.CASCADE)
-    adress = models.CharField(max_length = 100)
 
 
 # Create your models here.
