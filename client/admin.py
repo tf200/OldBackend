c77aad72e3edfb362845ac998b5a956215b66524
medram <mramouchy@gmail.com>
2024-04-02 10:54:37 +0000

    New models added to Django admin

--- client/admin.py
+++ client/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 
-from .models import CareplanAtachements
+from .models import (
+    CarePlan,
+    CareplanAtachements,
+    ClientTypeContactRelation,
+    Contact,
+    Invoice,
+    InvoiceContract,
+    TemporaryFile,
+)
 
 # class ClientDetailsAdmin(admin.ModelAdmin):
 #     # This is optional, to customize how the model appears in the admin
@@ -19,11 +27,38 @@ from .models import CareplanAtachements
 # admin.site.register(ClientDetails, ClientDetailsAdmin)
 
 
+@admin.register(CareplanAtachements)
 class CareplanAttachmentsAdmin(admin.ModelAdmin):
     list_display = ("id", "careplan", "name", "created_at")  # Customize columns displayed
     list_filter = ("created_at",)  # Enable filtering by these fields
     search_fields = ("name", "careplan__description")  # Enable search by these fields
 
 
-# Register your models here.
-admin.site.register(CareplanAtachements, CareplanAttachmentsAdmin)
+@admin.register(InvoiceContract)
+class CareplanAtachementsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(CarePlan)
+class InvoiceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TemporaryFile)
+class InvoiceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ClientTypeContactRelation)
+class InvoiceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Contact)
+class InvoiceAdmin(admin.ModelAdmin):
+    pass
--- employees/admin.py
+++ employees/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 
-from .models import EmployeeProfile, WeeklyReportSummary
+from .models import (
+    AiGeneratedWeeklyReports,
+    Certification,
+    ClientDetails,
+    ClientGoals,
+    ClientMedication,
+    EmotionalState,
+    EmployeeProfile,
+    Feedback,
+    GoalsReport,
+    Observations,
+    PhysicalState,
+    ProgressReport,
+    WeeklyReportSummary,
+)
 
 
 @admin.register(EmployeeProfile)
@@ -11,11 +25,52 @@ class EmployeeProfileAdmin(admin.ModelAdmin):
 
 
 # Optional: Define a custom admin class to customize the admin interface
+@admin.register(WeeklyReportSummary)
 class WeeklyReportSummaryAdmin(admin.ModelAdmin):
     list_display = ("client", "created_at")  # Customize the list display fields
     list_filter = ("created_at", "client")  # Enable filtering by these fields
     search_fields = ("summary_text",)  # Enable search by summary text
 
+@admin.register(Certification)
+class InvoiceAdmin(admin.ModelAdmin):
+    pass
 
-# Register the model and the admin class with the admin site
-admin.site.register(WeeklyReportSummary, WeeklyReportSummaryAdmin)
+@admin.register(AiGeneratedWeeklyReports)
+class InvoiceAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ClientDetails)
+class InvoiceAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ClientGoals)
+class InvoiceAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(EmotionalState)
+class InvoiceAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(GoalsReport)
+class InvoiceAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(PhysicalState)
+class InvoiceAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ProgressReport)
+class InvoiceAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ClientMedication)
+class InvoiceAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Feedback)
+class InvoiceAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Observations)
+class InvoiceAdmin(admin.ModelAdmin):
+    pass
