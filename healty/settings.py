0c2fbe79a28f2515637a32c974590259fbeb3ba5
medram <mramouchy@gmail.com>
2024-04-03 14:30:07 +0000

    Fixed loading .env file without/outside docker-compose

--- healty/settings.py
+++ healty/settings.py
@@ -15,9 +15,12 @@ from datetime import timedelta
 from pathlib import Path
 
 from celery.schedules import crontab
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
