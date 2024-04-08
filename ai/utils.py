d22e9c2886c45a812e128a0ee5fa854fba59ba12
medram <mramouchy@gmail.com>
2024-04-08 09:10:37 +0000

    enhance reports via ai :D

--- ai/utils.py
+++ ai/utils.py
@@ -1,3 +1,19 @@
-def ai_enhance_report(content: str) -> str:
+from django.conf import settings
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
 
-    return "enhanced report :D"
+
+def ai_enhance_report(content: str) -> str:
+    llm = ChatOpenAI(model=settings.OPENAI_MODEL, temperature=0, api_key=settings.OPENAI_KEY)
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "Please enhance the following text/report and make it more appealing."),
+            ("user", "{input}"),
+        ]
+    )
+
+    chain = prompt | llm | StrOutputParser()
+
+    return chain.invoke({"input": content})
