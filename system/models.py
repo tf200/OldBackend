85877de1c32b577c47f315117a071253a7111cd4
medram <mramouchy@gmail.com>
2024-05-03 10:18:03 +0100

    TAX has been added to Expenses

--- system/models.py
+++ system/models.py
@@ -210,7 +210,8 @@ class AttachmentFile(models.Model):
 
 
 class Expense(models.Model):
-    amount = models.DecimalField(max_digits=20, decimal_places=2)
+    amount = models.DecimalField(max_digits=20, decimal_places=2)  # amount without TAX
+    tax = models.IntegerField(default=0)
     desc = models.TextField(default="", null=True, blank=True)
     attachment_ids = models.JSONField(default=list, blank=True)
     updated = models.DateTimeField(auto_now=True)
