20b59e1eea47fadb95f69bdee074c49968a34a1a
medram <mramouchy@gmail.com>
2024-07-12 15:41:24 +0100

    Remove unnecessary configs

--- healty/settings.py
+++ healty/settings.py
@@ -184,16 +184,14 @@ if USE_S3:
     AWS_DEFAULT_ACL = os.getenv("AWS_DEFAULT_ACL", None)
     AWS_S3_REGION_NAME: str = os.getenv("AWS_S3_REGION_NAME", "us-east-2")  # e.g., us-east-2
     AWS_S3_CUSTOM_DOMAIN: str = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-    AWS_S3_HOST: str = os.getenv("AWS_S3_HOST", "s3.amazonaws.com")
-    AWS_AUTH_MECHANISM = 'AWS4-HMAC-SHA256'
-    
+
     # Static files (CSS, JavaScript, Images)
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
     STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
     # Media files
-    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
     MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 else:
     STATIC_URL = "/static/"
     MEDIA_URL = "/media/"
